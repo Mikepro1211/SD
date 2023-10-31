c54ff66f24f93d1548d9bb31ab6ec6a3646420de
Mikepro <miguelalexander890@gmail.com>
2023-10-30 18:28:04 -0600

    Nuevos cambios UI

--- src/App.js
+++ src/App.js
@@ -1,51 +1,46 @@
 import { Button } from 'reactstrap';
-
+import '../src/prueba.css';
 import { MenuTab } from './source/components/MenuTab';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styled from 'styled-components';
+import Col from 'reactstrap';
+
+
 //Imagenes
 import objeto from './source/img/objeto.png'
 import rpc  from './source/img/rpc.png'
 import p2p from  './source/img/2p2.png'
 import soa from  './source/img/SOA.jpg'
+import ContainerColumns from './ContainerColumns';
 function App() {
   return (
     <view>
+
+
     <MenuTab/>
-    <MenuLetter>
-    
-      
-      <img src={'https://pandorafms.com/blog/wp-content/uploads/2023/04/Sistemas-distribuidos-caracteristicas-y-mejores-practicas.jpg'}  alt="Responsive image" />
-
-
-    </MenuLetter>
-    <div>
-    
-    <h3>¿Que es la comunicacion en los sistemas distribuidos</h3>
-      <p id="comunication">Se le conoce como comunicacion dentro de un sistema distribuida al procreso que se 
-      encuentra en el nucleo de los sistemas distribuidos, es decir que la comunicacion generalmente sucede en el paso 
-      de mensaje a nivel bajo, el cual la comunicacion puede ser sincronica y asincronica </p>
-      <br></br>
-      <ol>
-        
-        <li>Comunicacion Sincrónica</li>
-        <p>Este tipo de comunicación tiene la principal funcion 
-          de que el remitente es desactivado o bloqueado hasta que se 
-          pueda obtener el estado de que la peticion ha sido aceptada.
-          <b>En pocas palabras Envio y recepcion de información se realizan de forma simultanea</b>
-        </p>
-        
-        <li>comunicación asincronica</li>
-        
-        <p>A lo contrario este envio no requiere que el receptor este esperando  , ya que este tipo de comunicacion es aquella que permite que el remitente
-          pueda tener las posibilidades de que continue inmediatamente despues de que ha pasado el mensaje. Lo que significa que el mensajea puede quedar en un 
-          estado de modo temporal
-        </p>
-      </ol>
-    </div>
-    </view>
+   
+    <div className="containers">
+      <div className='containerText'>
+     <h1 className='titulos'>¿Que es un sistema distribuido?</h1>
+     </div>
+     <img  className="imagenGrande"src={'https://dc722jrlp2zu8.cloudfront.net/media/uploads/2018/07/31/sistema-distribuido.png'}  alt="Responsive image" />
      
+    </div>
+
+    <div className="informacion">
+      <h1 className='titulos'>¿Que es un sistemas Distribuido?</h1>
+      <p className='paragraph'>Un sistema distribuido es un conjunto de programas informáticos que utilizan recursos computacionales en varios nodos de cálculo distintos para lograr un objetivo compartido común. La finalidad de los sistemas distribuidos es eliminar los cuellos de botella o los puntos de error centrales de un sistema.</p>
+      <p className='paragraph'>Un sistema distribuido es un conjunto de programas informáticos que utilizan recursos computacionales en varios nodos de cálculo distintos para lograr un objetivo compartido común. Este tipo de sistemas, también denominados "computación distribuida" o "bases de datos distribuidas", usan nodos distintos para comunicarse y sincronizarse a través de una red común. Estos nodos suelen representar dispositivos de hardware físicos diferentes, pero también pueden representar procesos de software diferentes u otros sistemas encapsulados recursivos. La finalidad de los sistemas distribuidos es eliminar los cuellos de botella o los puntos de error centrales de un sistema.</p>
+      
+    </div>
+    <div>
+     <h1 className='titulosBlack'>Caracteristicas de un sistema distribuido</h1>
     
+    <ContainerColumns/>
+    </div>
+      
+    </view>
+          
   );
   
 }
